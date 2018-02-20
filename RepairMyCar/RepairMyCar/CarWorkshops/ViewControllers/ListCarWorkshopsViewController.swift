3e0bed0b5040e8c70b5f77a0d99fa17f571e897f
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2018-02-19 23:15:46 -0300

    Adiciona mapa inicial ao projeto

---
+++ RepairMyCar/RepairMyCar/CarWorkshops/ViewControllers/ListCarWorkshopsViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import MapKit
+
+class ListCarWorkshopsViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(location: initialLocation)
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+}
